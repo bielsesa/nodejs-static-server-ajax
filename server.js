bf246d72c742a8b77cb66af0349c377d995d52ea
Biel Serrano Sanchez <bielsesa@gmail.com>
2019-12-20 12:30:00 +0100

    Format server.js

--- server.js
+++ server.js
@@ -2,24 +2,24 @@ const http = require('http');
 const url = require('url');
 
 const iniciar = (encaminar, manegadorPeticions) => {
-    const onReq = (req, res) => {
-        const pathname = url.parse(req.url).pathname;
-        console.log(`S'ha rebut una petició a ${pathname}`);
+        const onReq = (req, res) => {
+                const { pathname } = url.parse(req.url);
+                console.log(`S'ha rebut una petició a ${pathname}`);
 
-        let dataPOST = '';
-        req.setEncoding('utf8');
-        req.addListener('data', data => {
-            dataPOST += data;
-            console.log(`S'ha rebut part del POST: ${data}`);
-        })
+                let dataPOST = '';
+                req.setEncoding('utf8');
+                req.addListener('data', data => {
+                        dataPOST += data;
+                        console.log(`S'ha rebut part del POST: ${data}`);
+                });
 
-        req.addListener('end', () => {
-            encaminar(manegadorPeticions, pathname, res, dataPOST);        
-        });
-    }
+                req.addListener('end', () => {
+                        encaminar(manegadorPeticions, pathname, res, dataPOST);
+                });
+        };
 
-    http.createServer(onReq).listen(9000);
-    console.log('Iniciat servidor al port 9000');
-}
+        http.createServer(onReq).listen(9000);
+        console.log('Iniciat servidor al port 9000');
+};
 
 exports.iniciar = iniciar;
